2e7961e5de8f0c48b4aae31ddb0269c30b7df805
Karbob <karbobc@gmail.com>
2024-01-14 01:37:49 +0800

    feat: 修改vite配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,34 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import * as process from "process";
+import { UserConfig, defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+export default defineConfig((config: UserConfig) => {
+  // 加载环境变量
+  const env = loadEnv(config.mode, process.cwd());
+
+  return {
+    plugins: [tsconfigPaths(), react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_BASE_URL || "http://127.0.0.1:12312",
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+    build: {
+      outDir: "./dist",
+      emptyOutDir: true,
+      minify: "terser",
+      terserOptions: {
+        compress: {
+          drop_console: true,
+          drop_debugger: true,
+        },
+      },
+    },
+  };
 });
