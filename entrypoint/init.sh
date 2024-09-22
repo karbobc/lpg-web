4e0a2cdaf461790ceb7ddda561072b765022bbd2
Karbob <karbobc@gmail.com>
2024-09-22 12:12:30 +0800

    fix: init.sh

--- entrypoint/init.sh
+++ entrypoint/init.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-[ -z "$LPG_WEB_TITLE" ] || sed -i "s|\(<title>\).*\(</title>\)|\1${LPG_WEB_TITLE}\2|p" /usr/share/nginx/html/index.html
+[ -z "$LPG_WEB_TITLE" ] || sed -i "s|\(<title>\).*\(</title>\)|\1${LPG_WEB_TITLE}\2|g" /usr/share/nginx/html/index.html
